6dc8fea6a5ce10c02d650235e440411cf60a693e
iamdusan <dusnen@gmail.com>
2020-12-16 22:34:28 +0100

    added method for chossing address from dropdown. added junit selectors for searchBox and orderButton

--- src/test/java/pages/DonesiHomePage.java
+++ src/test/java/pages/DonesiHomePage.java
@@ -4,10 +4,18 @@ import helpers.BaseHelper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class DonesiHomePage extends BaseHelper
 {
+    @FindBy(className = "address-form-component-search-input")
+    WebElement searchBox;
+
+    @FindBy(className = "btn-primary")
+    WebElement orderButton;
+
     public DonesiHomePage(WebDriver driver)
     {
         this.driver = driver;
@@ -21,14 +29,21 @@ public class DonesiHomePage extends BaseHelper
 
     private void insertTextInsideSearchBox(String text)
     {
-        WebElement searchBox = driver.findElement(By.className("address-form-component-search-input"));
         searchBox.sendKeys(text);
+    }
+
+    private void chooseAddressFromDropDown()
+    {
+        wdWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\'address-form-component\']/div[2]/div[1]/ul/li[1]/span[3]/span[2]")));
+        WebElement dropDownList = driver.findElement(By.xpath("//*[@id=\'address-form-component\']/div[2]/div[1]/ul/li[1]/span[3]/span[2]"));
+        dropDownList.click();
     }
 
     public void donesiRestaurantsResults(String url, String text)
     {
         navigateToDonesi(url);
         insertTextInsideSearchBox(text);
+        chooseAddressFromDropDown();
     }
 
 
