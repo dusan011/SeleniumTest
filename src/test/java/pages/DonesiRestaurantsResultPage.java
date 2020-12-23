9501cdb779912c3a90bf670939989ff144bff816
iamdusan <dusnen@gmail.com>
2020-12-23 14:27:04 +0100

    added function for finding number of italian restaurants in header and result list. added assert in test class

--- src/test/java/pages/DonesiRestaurantsResultPage.java
+++ src/test/java/pages/DonesiRestaurantsResultPage.java
@@ -9,11 +9,16 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import java.util.List;
+
 public class DonesiRestaurantsResultPage extends BaseHelper
 {
 
     @FindBy(id = "italijanska-hrana")
     WebElement italianFoodCheckBoxFilter;
+
+    public int numberOfItalianRestaurantsInHeader = 0;
+    public int numberOfItalianRestaurantsInResults = 0;
 
     public DonesiRestaurantsResultPage(WebDriver driver)
     {
@@ -27,6 +32,24 @@ public class DonesiRestaurantsResultPage extends BaseHelper
         js.executeScript("arguments[0].click();", italianFoodCheckBoxFilter);
     }
 
+    private void findNumberOfItalianRestaurantsInHeader()
+    {
+        WebElement allRestaurants = driver.findElement(By.className("shops-listing-counter"));
+        System.out.println("Number of all restaurants is "+allRestaurants.getText());
+        wdWait.until(ExpectedConditions.invisibilityOfElementWithText(By.className("shops-listing-counter"),allRestaurants.getText()));
+        WebElement italianRestaurantsInHeader = driver.findElement(By.className("shops-listing-counter"));
+        numberOfItalianRestaurantsInHeader = Integer.parseInt(italianRestaurantsInHeader.getText());
+        System.out.println("Number of Italian restaurants is "+italianRestaurantsInHeader.getText());
+    }
+
+    private void findNumberOfItalianRestaurantsInResults()
+    {
+        List<WebElement> italianRestaurantsInResultPage = driver.findElements(By.className("shops-listings-shops-list-item"));
+        numberOfItalianRestaurantsInResults = italianRestaurantsInResultPage.size();
+        System.out.println("Number of Italian restaurants in results is "+italianRestaurantsInResultPage.size());
+    }
+
+
     private void getNumberOfRestaurants()
     {
         wdWait.until(ExpectedConditions.invisibilityOfElementWithText(By.className("shops-listing-counter"),""));
@@ -38,5 +61,7 @@ public class DonesiRestaurantsResultPage extends BaseHelper
     {
         getNumberOfRestaurants();
         clickOnItalianFoodCheckBoxFilter();
+        findNumberOfItalianRestaurantsInHeader();
+        findNumberOfItalianRestaurantsInResults();
     }
 }
--- src/test/java/tests/DonesiTest.java
+++ src/test/java/tests/DonesiTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 import pages.DonesiHomePage;
 import pages.DonesiRestaurantsResultPage;
@@ -12,6 +13,7 @@ public class DonesiTest extends BaseTest
        donesi.donesiRestaurantsResults("http://www.donesi.com", "Beogradska 10");
        DonesiRestaurantsResultPage restaurantsPage = new DonesiRestaurantsResultPage(driver);
        restaurantsPage.donesiRestaurantsResult();
+       Assert.assertTrue("Number of restaurants in header and result page is not the same", restaurantsPage.numberOfItalianRestaurantsInHeader == restaurantsPage.numberOfItalianRestaurantsInResults);
        Thread.sleep(5000);
    }
 }
