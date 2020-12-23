ae50476cecabda07805d2cc62f0b602901a86bee
iamdusan <dusnen@gmail.com>
2020-12-23 13:46:21 +0100

    added function for selecting "italijanski-restorani" filter

--- src/test/java/pages/DonesiRestaurantsResultPage.java
+++ src/test/java/pages/DonesiRestaurantsResultPage.java
@@ -2,11 +2,31 @@ package pages;
 
 import helpers.BaseHelper;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class DonesiRestaurantsResultPage extends BaseHelper
 {
+
+    @FindBy(id = "italijanska-hrana")
+    WebElement italianFoodCheckBoxFilter;
+
+    public DonesiRestaurantsResultPage(WebDriver driver)
+    {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    private void clickOnItalianFoodCheckBoxFilter()
+    {
+        wdWait.until(ExpectedConditions.invisibilityOfElementWithText(By.className("shops-listing-counter"),""));
+        js.executeScript("arguments[0].click();", italianFoodCheckBoxFilter);
+    }
+
     private void getNumberOfRestaurants()
     {
         wdWait.until(ExpectedConditions.invisibilityOfElementWithText(By.className("shops-listing-counter"),""));
@@ -17,5 +37,6 @@ public class DonesiRestaurantsResultPage extends BaseHelper
     public void donesiRestaurantsResult()
     {
         getNumberOfRestaurants();
+        clickOnItalianFoodCheckBoxFilter();
     }
 }
--- src/test/java/tests/DonesiTest.java
+++ src/test/java/tests/DonesiTest.java
@@ -9,8 +9,8 @@ public class DonesiTest extends BaseTest
    @Test
     public void donesiTest() throws InterruptedException {
        DonesiHomePage donesi = new DonesiHomePage(driver);
-       donesi.donesiRestaurantsResults("http://www.donesi.com", "Balkanska 1");
-       DonesiRestaurantsResultPage restaurantsPage = new DonesiRestaurantsResultPage();
+       donesi.donesiRestaurantsResults("http://www.donesi.com", "Beogradska 10");
+       DonesiRestaurantsResultPage restaurantsPage = new DonesiRestaurantsResultPage(driver);
        restaurantsPage.donesiRestaurantsResult();
        Thread.sleep(5000);
    }
